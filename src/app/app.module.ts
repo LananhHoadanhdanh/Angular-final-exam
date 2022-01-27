cefe95ffb7c07c7b832f7d808e5344dc2d74ad7e
Phạm Thị Lan Anh <90228386+LananhHoadanhdanh@users.noreply.github.com>
2022-01-27 09:10:58 +0700

    tạo khung cơ bản

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookManagementComponent } from './component/book-management/book-management.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookManagementComponent
   ],
   imports: [
     BrowserModule,
